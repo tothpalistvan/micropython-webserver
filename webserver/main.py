d2fa1641c2f196750cc1fe53c91a17fe45575a70
tothpalistvan <102403786+tothpalistvan@users.noreply.github.com>
2022-03-26 23:00:42 +0100

    Handling config, prints time

--- webserver/main.py
+++ webserver/main.py
@@ -11,7 +11,11 @@ myconnection = myWifiConnection( myconfig )
 
 # 3, Start Webserver and handle requests
 from mywebserver import myWebServer
-myserver = myWebServer( myconnection )
+myserver = myWebServer( myconnection, myconfig )
+
+import utime
+print("Current time: %04d-%02d-%02d %02d:%02d:%02d" % utime.localtime()[:6] )
+
 myserver.start()
 
 

c537138a0b3963f5d29bb6fcce072bcaef29d0fe
tothpalistvan <102403786+tothpalistvan@users.noreply.github.com>
2022-03-26 08:34:14 +0100

    Update mywebserver.py
    Implemented GET and POST
    Added basic py file support and handle  http://ip/file.py.html

--- webserver/mywebserver.py
+++ webserver/mywebserver.py
@@ -1,10 +1,7 @@
 # mywebserver.py - micropython-webserver - free sotware under GNU GPL
 # Author: (c)2022 Tóthpál István <istvan@tothpal.eu>
 
-#
-#   Now it can only serve HTML files, maybe it would be connected with custom python queries/MCU commands
-
-import uos,socket
+import uos,usocket as socket,sys,gc,time
 
 class myWebServer:
 
@@ -12,6 +9,7 @@ class myWebServer:
         self.connection = connection
         self.HTMLBasePath = "/html"
         self.DefaultFile = "index.html"
+        self.HTTPRequestData = {}
         message = ''
         
         if not self.connection.isconnected():
@@ -23,8 +21,42 @@ class myWebServer:
         print("myWebServer init...", message)
 
     def explode_data(self, data):
-        tmp = str(data).split("\\r\\n")[0].split()
-        return tmp[1].split("?")
+        """Explode HTTP Request Data
+           Returns URI path + URIFile"""
+        result = {}
+        rows =  str(data).split("'")[1].split("\\r\\n")
+        i = -1
+        for row in rows:
+            items = row.split(': ')
+            if len(items)==1 and row!="":
+                item = items[0].split()
+                if item[0] in ['GET','HEAD','POST','PUT','DELETE','CONNECT','OPTIONS','TRACE','PATCH']:
+                    result['Method'] = item[0]
+                    result['URI'] = item[1]
+                    result['HTTPver'] = item[2]
+            elif len(items)==2:
+                result[items[0]] = items[1]
+            else:
+                if row!="":
+                    result[str(i)] = row
+                    i -= 1
+        self.HTTPRequestData = result
+
+        poststart = str(data).rfind("\\r\\n")
+        if poststart+4 < len(str(data)):
+            self.HTTPRequestData['_POST_'] = str(data)[poststart+4:-1]
+
+        if 'URI' in self.HTTPRequestData.keys():
+            uri = self.HTTPRequestData['URI'].split('?')
+            if isinstance(uri,list):
+                self.HTTPRequestData['URIFile'] = uri[0]
+                if len(uri)==2:
+                     self.HTTPRequestData['URLQuery'] = uri[1]
+            else:
+                self.HTTPRequestData['URIFile'] = uri
+                del self.HTTPRequestData['URLQuery']
+            return self.HTTPRequestData['URIFile']
+        return ''
 
     def fileExists(self,filename):
         try:
@@ -39,42 +71,115 @@ class myWebServer:
         except OSError:
            return False
 
-    def start(self):
-        addr = socket.getaddrinfo( self.connection.get_address(), 80)[0][-1]
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(addr)
-        s.listen(5)
+    
+    def start(self):       
+#        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.mysock = socket.socket()
+        
+        ai = socket.getaddrinfo(self.connection.get_address(), 80)
+        addr = ai[0][-1]
 
+        self.mysock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.mysock.bind(addr)
+        self.mysock.listen(5)
         while True:
-          conn, addr = s.accept() # addr = connection from address
-          request = conn.recv(1024)
+          res = self.mysock.accept()
+          conn = res[0]
+          client_addr = res[1]
+
+          req = conn.readline()
+          retval = req
+          while True:
+              h = conn.readline()
+              retval += h
+              if h == b"" or h == b"\r\n":
+                    break
+
+          request = retval
+          
+          #print(request)
+          status = "ok"
+          response = ""
+          message = ""
           if request:
-              path = self.explode_data(request) 
-              accept = str(request).split("\\r\\n")[3].split(": ")[1].split(",")
-        
-              if isinstance(path,list):
-                filename = self.HTMLBasePath + path[0]
-              else: 
-                filename = self.HTMLBasePath + path
-              
-              if self.isdir(filename):
-                if filename[-1] != "/":
-                      filename += "/"
-                filename = filename+self.DefaultFile
-                  
-              if self.fileExists(filename): 
-                  f = open( filename )
-                  response = f.read()
-                  f.close()
-        
-                  conn.send('HTTP/1.1 200 OK\n')
-                  conn.send('Content-Type: '+accept[0]+'\n')
-                  conn.send('Connection: close\n\n')
-                  conn.sendall(response)
+              uri = self.explode_data(request)
+              if 'Method' in self.HTTPRequestData.keys() and status=="ok":
+                  if self.HTTPRequestData['Method'] not in ['GET','POST']:
+                      status = "error"
+                      response = "405 Method Not Allowed"
+                      message = "HTTP/1.1 405 Method Not Allowed\n"
+                  if self.HTTPRequestData['Method']=='POST':
+                      #receive post data Content-Length
+                      retval = conn.read(eval(self.HTTPRequestData['Content-Length']))
+                      self.HTTPRequestData['_POST_'] = retval.decode()
               else:
-                  conn.send('HTTP/1.1 404 Not Found\n')
-                  conn.send('Content-Type: text/html\n')
-                  conn.send('Connection: close\n\n')
-                  conn.sendall('HTTP 404 - File not found!')
+                      status = "error"
+                      response = "405 Method Not Allowed"
+                      message = "HTTP/1.1 405 Method Not Allowed\n"
+
+              if 'HTTPver' in self.HTTPRequestData.keys() and status=="ok":
+                  if self.HTTPRequestData['HTTPver'] not in ['HTTP/1.0','HTTP/1.1']:
+                      status = "error"
+                      response = "505 HTTP Version Not Supported"
+                      message = "HTTP/1.1 505 HTTP Version Not Supported\n"
+
+#              print(len(request),len(retval),self.HTTPRequestData)
+
+              if status=="ok":      
+                accept = self.HTTPRequestData['Accept'].split(',')
+        
+                filename = self.HTMLBasePath + uri
+              
+                if self.isdir(filename):
+                  if filename[-1] != "/":
+                      filename += "/"
+                  filename = filename+self.DefaultFile
+                else:
+                  if 'Referer' in self.HTTPRequestData.keys():
+                    refer = self.HTTPRequestData['Referer'].split(self.HTTPRequestData['Host'])
+                    if not self.fileExists(filename) and len(refer)==2:
+                        if self.isdir(refer[1].split('?')[0]):
+                            filename = self.HTMLBasePath + refer[1].split('?')[0] + uri
+                        else:
+                            cutpoint = refer[1].split('?')[0].rfind("/")
+                            filename = self.HTMLBasePath + refer[1].split('?')[0][:cutpoint] + uri
+
+                if filename[-8:] == ".py.html":
+                  filename = filename[:-5]
+                if self.fileExists(filename): 
+                  f = open( filename )
+                  myfiledata = f.read()
+                  f.close()
+                  if filename[-3:] == ".py":
+                    #print( self.HTTPRequestData )
+                    cutpoint = filename.rfind("/")
+                    path = filename[:cutpoint]
+                    pyfile = filename[cutpoint+1:-3] 
+                    sys.path.append(path)
+                    mymodules = __import__(pyfile, globals(), locals(), [], 0)
+#                    print(mymodules)
+                    pyhtml = mymodules.myPYHTMLContent(self.HTTPRequestData, path, pyfile)
+                    sys.path.remove(path)
+                    r = pyhtml.doMCUThings()
+                    #if r==False:
+                    response = pyhtml.generate()
+                    del sys.modules[pyfile]
+                    gc.collect()
+                  else:
+                    response = myfiledata
+                    
+                  message = 'HTTP/1.1 200 OK\n'
+                else:       
+                  message = 'HTTP/1.1 404 Not Found\n'
+                  response = 'HTTP 404 - File not found!'
+
+              conn.write(message.encode())
+              if status=="ok":
+                  conn.write(('Content-Type: '+accept[0]+'\n').encode())
+              else:
+                  conn.write('Content-Type: text/html\n'.encode())
+              conn.write( ('Content-Length: {}\n').format(len(response.encode())).encode())
+              conn.write('Connection: close\n\n'.encode())
+              conn.write(response.encode())
                   
           conn.close()
